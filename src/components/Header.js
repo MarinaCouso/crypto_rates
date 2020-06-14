f7d2c72ce35bf2f4a2207259091102c213ca8656
MarinaCouso <marina.couso.martinez@gmail.com>
2020-06-14 13:26:58 +0200

    #2 Header structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import Logo from '../images/logo.svg';
 
 function Header() {
-  return <>Hello wonderful world</>;
+  return (
+    <header>
+      <nav class='header__nav'>
+        <div class='header__nav__logo'>
+          <img src={Logo} alt="Monerdero's logo" title="Monerdero's logo" />
+        </div>
+        <ul class='header__nav__menu'>
+          <li>
+            <a href='https://www.monedero.com/seguridad' target='_blank'>
+              Seguridad
+            </a>
+          </li>
+          <li>
+            <a href='https://www.monedero.com/sobre-nosotros' target='_blank'>
+              Sobre nosotros
+            </a>
+          </li>
+          <li>
+            <a href='https://www.monedero.com/faq' target='_blank'>
+              FAQ
+            </a>
+          </li>
+          <li>
+            <a href='https://blog.monedero.com/' target='_blank'>
+              Blog
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class='header__hero'>
+        <div class='header__hero__text'>
+          <h1>Compara precios rápido y fácil</h1>
+          <p>Elige las monedas que quieres comparar y la cantidad que quieres calcular.</p>
+          <p>Así de sencillo.</p>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
