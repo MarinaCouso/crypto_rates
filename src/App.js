d7a38efd461063d0ff88e6b2d0520240d55fd1c3
MarinaCouso <marina.couso.martinez@gmail.com>
2020-06-14 15:08:32 +0200

    #2 Add Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Form from './components/Form';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Form />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Logo from '../images/logo.svg';
+import Twitter from '../images/twitter.svg';
+
+function Footer() {
+  return (
+    <footer>
+      <div className='footer__text'>
+        <img src={Logo} />
+        <p>Recibe, envía e intercambia tus criptomonedas de la manera más sencilla. Sin líos ni comisiones. </p>
+        <a href='https://twitter.com/monederodotcom'>
+          <img src={Twitter} />
+        </a>
+      </div>
+      <nav className='footer__nav'>
+        <div className='footer__nav__1'>
+          <h6>Navegación</h6>
+          <ul>
+            <li>Seguridad</li>
+            <li>Sobre nosotros</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div className='footer__nav__2'>
+          <h6>Legal</h6>
+          <ul>
+            <li>Términos y condiciones</li>
+          </ul>
+        </div>
+        <div className='footer__nav__3'>
+          <h6>Contacto</h6>
+          <ul>
+            <li>Soporte</li>
+          </ul>
+        </div>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
