5c4a8031bf95b95a311f8181db4e48f51410d2c5
MarinaCouso <marina.couso.martinez@gmail.com>
2020-06-14 22:32:02 +0200

    Run docs

---
+++ docs/build/precache-manifest.69b1ddd944eadbfc721325b812303c0b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43396db8e06ede0c56c0730a60d1f313",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d928d87056e4f96a713",
+    "url": "./static/css/main.c59cc0a9.chunk.css"
+  },
+  {
+    "revision": "e27c215d75c61531010e",
+    "url": "./static/js/2.fce8a297.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fce8a297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d928d87056e4f96a713",
+    "url": "./static/js/main.5d9644b2.chunk.js"
+  },
+  {
+    "revision": "eb6b88b618434723e508",
+    "url": "./static/js/runtime-main.76966c85.js"
+  },
+  {
+    "revision": "274d44f020650953069e6daaf4929b8b",
+    "url": "./static/media/bch.274d44f0.svg"
+  },
+  {
+    "revision": "ba15dfd4a0d6798a4529242ea66d2eb2",
+    "url": "./static/media/btc.ba15dfd4.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "./static/media/eth.18708d3e.svg"
+  },
+  {
+    "revision": "96af4dd93e2abae4dca19e998d664193",
+    "url": "./static/media/logo.96af4dd9.svg"
+  },
+  {
+    "revision": "3d55f945937ed2c566a743996e74e9e0",
+    "url": "./static/media/ltc.3d55f945.svg"
+  },
+  {
+    "revision": "73896d670f28ee5b7d097c10bd872e8a",
+    "url": "./static/media/tusd.73896d67.svg"
+  },
+  {
+    "revision": "b058414c62ec9d704b241c2b499eb686",
+    "url": "./static/media/twitter.b058414c.svg"
+  },
+  {
+    "revision": "a0d924d84fea6c805b7f560c34aea341",
+    "url": "./static/media/usdc.a0d924d8.svg"
+  },
+  {
+    "revision": "66f1b14738c83efe0e08e5c27b7d54ee",
+    "url": "./static/media/usdt.66f1b147.svg"
+  },
+  {
+    "revision": "760c27f5376fb9b716fb590e597d5f16",
+    "url": "./static/media/wallet-resized.760c27f5.jpg"
+  }
+]);
