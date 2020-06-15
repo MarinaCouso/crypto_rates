42d5f02d8eddd24683e5de212f4f2befd9dc0b9a
MarinaCouso <marina.couso.martinez@gmail.com>
2020-06-15 18:22:02 +0200

    #2 Adjust Chart styles to desktop version - mobile pending

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -33,24 +33,27 @@ export default class Chart extends PureComponent {
 
   render() {
     return (
-      <BarChart
-        width={800}
-        height={500}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray='3' />
-        <XAxis dataKey='name' />
-        <YAxis />
-        {/* <Tooltip /> */}
-        {/* <Legend /> */}
-        <Bar dataKey='quantity' fill='#82ca9d' />
-      </BarChart>
+      <div className='chart'>
+        <BarChart
+          width={800}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          className='chart__bar'
+        >
+          <CartesianGrid strokeDasharray='3' />
+          <XAxis dataKey='name' />
+          <YAxis />
+          {/* <Tooltip /> */}
+          {/* <Legend /> */}
+          <Bar dataKey='quantity' fill='#82ca9d' />
+        </BarChart>
+      </div>
     );
   }
 }
