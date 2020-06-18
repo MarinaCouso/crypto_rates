3e5f2fc6a0f5806b96af2adbe6e29d511a074c5e
MarinaCouso <marina.couso.martinez@gmail.com>
2020-06-18 16:11:11 +0200

    Corect fetch url

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,7 +1,8 @@
 const getDataFromApi = async (search) => {
-  const response = await fetch('http://compare.monedero.com/api/getPrice?pair=' + search.pair + '&amount=' + search.amount);
+  const response = await fetch('//compare.monedero.com/api/getPrice?pair=' + search.pair + '&amount=' + search.amount);
   const data = await response.json();
   return data;
 };
 
 export default getDataFromApi;
+ce;
